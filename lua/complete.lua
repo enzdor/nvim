f88fb08cdb8d7d31af61d3c5b49244f1e9b885c2
enzdor <enzo01lb@gmail.com>
2023-09-24 19:02:05 +0200

    change bindings

--- lua/complete.lua
+++ lua/complete.lua
@@ -19,7 +19,7 @@ cmp.setup({
     --     ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-e>'] = cmp.mapping.abort(),
 	['<C-Space>'] = cmp.mapping.complete(),
-	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	-- ['<CR>'] = cmp.mapping.confirm({ select = true }),
 	["<Tab>"] = cmp.mapping(function(fallback)
 	      if cmp.visible() then
 		cmp.select_next_item()
