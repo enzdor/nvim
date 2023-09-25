e032a51096edd3f9a5ef538f4e212cfb9f605020
enzdor <enzo01lb@gmail.com>
2023-09-25 14:50:51 +0200

    comment out snippets for groff

--- lua/luasnippets/all.lua
+++ lua/luasnippets/all.lua
@@ -25,192 +25,192 @@ local types = require("luasnip.util.types")
 local parse = require("luasnip.util.parser").parse_snippet
 
 
-return {
-    s({trig = "gr", dscr = "Create a graph"},
-	fmta([[
-	    .PS
-	    Y: box width 0 height 0 invis at (0, <>)
-	    X: box width 0 height 0 invis at (<>, 0)
-	    O: box width 0 height 0 invis at (0, 0)
-	    YL: box "<>" invis at (-0.25 , <>)
-	    XL: box "<>" invis at (<> , -0.25)
-	    arrow from O.n to Y.n
-	    arrow from O.n to X.n
-	    .PE
-	]],
-	{i(1), i(2), i(3), rep(1), i(4) ,rep(2)}
-	)
-    ),
-    s({trig = "ln", dscr = "Create a line for a graph"},
-	fmta([[
-	    <>A: box width 0 height 0 invis at (<>, <>)
-	    <>B: box width 0 height 0 invis at (<>, <>)
-	    line from <>A.n to <>B.n
-	]],
-	{i(1), i(2), i(3), rep(1), i(4), i(5), rep(1), rep(1)}
-	)
-    ),
-    s({trig = "lb", dscr = "Create a label for a graph"},
-	fmta([[
-	    <>L: box "<>" invis at (<>, <>)
-	]],
-	{i(1), i(2), i(3), i(4)}
-	)
-    ),
-    s({trig = "lp", dscr = "Create a point for a label"},
-	fmta([[
-	    <>P: circle fill radius 0.025 at (<>, <>)
-	]],
-	{i(1), i(2), i(3)}
-	)
-    ),
-    s({trig = "eq", dscr = "Create range for mathematical notation"},
-	fmta([[
-	    .EQ L
-	    <>
-	    .EN
-	]],
-	{i(0)}
-	)
-    ),
-    s({trig = "ff", dscr = "Create notation for fraction"},
-	fmta([[
-	    { <> } over { <> }
-	]],
-	{i(1), i(2)}
-	)
-    ),
-    s({trig = "ss", dscr = "Create notation for summation"},
-	fmta([[
-	    sum from { <> } to { <> }
-	]],
-	{i(1), i(2)}
-	)
-    ),
-    s({trig = "pp", dscr = "Create notation for parenthesis"},
-	fmta([[
-	    left ( <> right )
-	]],
-	{i(1)}
-	)
-    ),
-    s({trig = "bb", dscr = "Create a box"},
-	fmta([[
-	    <>: box "<>" invis at (<>, <>)
-	]],
-	{i(1), i(2), i(3), i(4)}
-    )
-    ),
-    s({trig = "bu", dscr = "Create a bullet point"},
-	fmta([[
-	    .IP <>
-	    <>
-	]],
-	{i(1, "\\(bu"), i(0)}
-	)
-    ),
-    s({trig = "bo", dscr = "Create bold text"},
-	fmta([[
-	    .B "<>" <>
-	    <>
-	    ]],
-	{i(1), i(2, ":"), i(0)}
-	)
-    ),
-    s({trig = "tb(%d)", dscr = "Make table", regTrig = true},
-	fmt([[
-	    .TS
-	    box, tab(>), center;
-	    []
-	    .TE
-	]],
-	{d(1,
-	function(args, snip)
-	    local nodes = {}
-	    local number = tonumber(snip.captures[1])
-	    if number ~= 0 then
-		for j = 1, number do
-		    if j == number then
-			table.insert(nodes, i(j, "c"))
-			table.insert(nodes, t({".", ""}))
-			for k = 1, number do
-			    if k == number then
-				table.insert(nodes, i(k + j, " "))
-			    else
-				table.insert(nodes, i(k + j, " "))
-				table.insert(nodes, t(">"))
-			    end
-			end
-		    else
-			table.insert(nodes, i(j, "c"))
-			table.insert(nodes, t(" "))
-		    end
-		end
-		return sn(nil, nodes)
-	    else
-	    end
-	end
-	)},
-	{delimiters = "[]"}
-	)
-    ),
-    s({trig = "tc(%d)", dscr = "Make changes to table style", regTrig = true},
-	fmt([[
-	    .T&
-	    []
-	]],
-	{d(1,
-	function(args, snip)
-	    local nodes = {}
-	    local number = tonumber(snip.captures[1])
-	    if number ~= 0 then
-		for j = 1, number do
-		    if j == number then
-			table.insert(nodes, i(j, "c"))
-			table.insert(nodes, t({".", ""}))
-			for k = 1, number do
-			    if k == number then
-				table.insert(nodes, i(k + j, " "))
-			    else
-				table.insert(nodes, i(k + j, " "))
-				table.insert(nodes, t(">"))
-			    end
-			end
-		    else
-			table.insert(nodes, i(j, "c"))
-			table.insert(nodes, t(" "))
-		    end
-		end
-		return sn(nil, nodes)
-	    else
-	    end
-	end
-	)},
-	{delimiters = "[]"}
-	)
-    ),
-    s({trig = "tl(%d)", dscr = "Create table line", regTrig = true},
-	{d(1,
-	function(args, snip)
-	    local nodes = {}
-	    local number = tonumber(snip.captures[1])
-	    if number ~= 0 then
-		for j = 1, number do
-		    if j == number then
-			table.insert(nodes, i(j, " "))
-		    else
-			table.insert(nodes, i(j, " "))
-			table.insert(nodes, t(">"))
-		    end
-		end
-		return sn(nil, nodes)
-	    else
-	    end
-	end
-	)},
-	{delimiters = "[]"}
-    ),
-}
+-- return {
+--     s({trig = "gr", dscr = "Create a graph"},
+-- 	fmta([[
+-- 	    .PS
+-- 	    Y: box width 0 height 0 invis at (0, <>)
+-- 	    X: box width 0 height 0 invis at (<>, 0)
+-- 	    O: box width 0 height 0 invis at (0, 0)
+-- 	    YL: box "<>" invis at (-0.25 , <>)
+-- 	    XL: box "<>" invis at (<> , -0.25)
+-- 	    arrow from O.n to Y.n
+-- 	    arrow from O.n to X.n
+-- 	    .PE
+-- 	]],
+-- 	{i(1), i(2), i(3), rep(1), i(4) ,rep(2)}
+-- 	)
+--     ),
+--     s({trig = "ln", dscr = "Create a line for a graph"},
+-- 	fmta([[
+-- 	    <>A: box width 0 height 0 invis at (<>, <>)
+-- 	    <>B: box width 0 height 0 invis at (<>, <>)
+-- 	    line from <>A.n to <>B.n
+-- 	]],
+-- 	{i(1), i(2), i(3), rep(1), i(4), i(5), rep(1), rep(1)}
+-- 	)
+--     ),
+--     s({trig = "lb", dscr = "Create a label for a graph"},
+-- 	fmta([[
+-- 	    <>L: box "<>" invis at (<>, <>)
+-- 	]],
+-- 	{i(1), i(2), i(3), i(4)}
+-- 	)
+--     ),
+--     s({trig = "lp", dscr = "Create a point for a label"},
+-- 	fmta([[
+-- 	    <>P: circle fill radius 0.025 at (<>, <>)
+-- 	]],
+-- 	{i(1), i(2), i(3)}
+-- 	)
+--     ),
+--     s({trig = "eq", dscr = "Create range for mathematical notation"},
+-- 	fmta([[
+-- 	    .EQ L
+-- 	    <>
+-- 	    .EN
+-- 	]],
+-- 	{i(0)}
+-- 	)
+--     ),
+--     s({trig = "ff", dscr = "Create notation for fraction"},
+-- 	fmta([[
+-- 	    { <> } over { <> }
+-- 	]],
+-- 	{i(1), i(2)}
+-- 	)
+--     ),
+--     s({trig = "ss", dscr = "Create notation for summation"},
+-- 	fmta([[
+-- 	    sum from { <> } to { <> }
+-- 	]],
+-- 	{i(1), i(2)}
+-- 	)
+--     ),
+--     s({trig = "pp", dscr = "Create notation for parenthesis"},
+-- 	fmta([[
+-- 	    left ( <> right )
+-- 	]],
+-- 	{i(1)}
+-- 	)
+--     ),
+--     s({trig = "bb", dscr = "Create a box"},
+-- 	fmta([[
+-- 	    <>: box "<>" invis at (<>, <>)
+-- 	]],
+-- 	{i(1), i(2), i(3), i(4)}
+--     )
+--     ),
+--     s({trig = "bu", dscr = "Create a bullet point"},
+-- 	fmta([[
+-- 	    .IP <>
+-- 	    <>
+-- 	]],
+-- 	{i(1, "\\(bu"), i(0)}
+-- 	)
+--     ),
+--     s({trig = "bo", dscr = "Create bold text"},
+-- 	fmta([[
+-- 	    .B "<>" <>
+-- 	    <>
+-- 	    ]],
+-- 	{i(1), i(2, ":"), i(0)}
+-- 	)
+--     ),
+--     s({trig = "tb(%d)", dscr = "Make table", regTrig = true},
+-- 	fmt([[
+-- 	    .TS
+-- 	    box, tab(>), center;
+-- 	    []
+-- 	    .TE
+-- 	]],
+-- 	{d(1,
+-- 	function(args, snip)
+-- 	    local nodes = {}
+-- 	    local number = tonumber(snip.captures[1])
+-- 	    if number ~= 0 then
+-- 		for j = 1, number do
+-- 		    if j == number then
+-- 			table.insert(nodes, i(j, "c"))
+-- 			table.insert(nodes, t({".", ""}))
+-- 			for k = 1, number do
+-- 			    if k == number then
+-- 				table.insert(nodes, i(k + j, " "))
+-- 			    else
+-- 				table.insert(nodes, i(k + j, " "))
+-- 				table.insert(nodes, t(">"))
+-- 			    end
+-- 			end
+-- 		    else
+-- 			table.insert(nodes, i(j, "c"))
+-- 			table.insert(nodes, t(" "))
+-- 		    end
+-- 		end
+-- 		return sn(nil, nodes)
+-- 	    else
+-- 	    end
+-- 	end
+-- 	)},
+-- 	{delimiters = "[]"}
+-- 	)
+--     ),
+--     s({trig = "tc(%d)", dscr = "Make changes to table style", regTrig = true},
+-- 	fmt([[
+-- 	    .T&
+-- 	    []
+-- 	]],
+-- 	{d(1,
+-- 	function(args, snip)
+-- 	    local nodes = {}
+-- 	    local number = tonumber(snip.captures[1])
+-- 	    if number ~= 0 then
+-- 		for j = 1, number do
+-- 		    if j == number then
+-- 			table.insert(nodes, i(j, "c"))
+-- 			table.insert(nodes, t({".", ""}))
+-- 			for k = 1, number do
+-- 			    if k == number then
+-- 				table.insert(nodes, i(k + j, " "))
+-- 			    else
+-- 				table.insert(nodes, i(k + j, " "))
+-- 				table.insert(nodes, t(">"))
+-- 			    end
+-- 			end
+-- 		    else
+-- 			table.insert(nodes, i(j, "c"))
+-- 			table.insert(nodes, t(" "))
+-- 		    end
+-- 		end
+-- 		return sn(nil, nodes)
+-- 	    else
+-- 	    end
+-- 	end
+-- 	)},
+-- 	{delimiters = "[]"}
+-- 	)
+--     ),
+--     s({trig = "tl(%d)", dscr = "Create table line", regTrig = true},
+-- 	{d(1,
+-- 	function(args, snip)
+-- 	    local nodes = {}
+-- 	    local number = tonumber(snip.captures[1])
+-- 	    if number ~= 0 then
+-- 		for j = 1, number do
+-- 		    if j == number then
+-- 			table.insert(nodes, i(j, " "))
+-- 		    else
+-- 			table.insert(nodes, i(j, " "))
+-- 			table.insert(nodes, t(">"))
+-- 		    end
+-- 		end
+-- 		return sn(nil, nodes)
+-- 	    else
+-- 	    end
+-- 	end
+-- 	)},
+-- 	{delimiters = "[]"}
+--     ),
+-- }
 
 
 
--- lua/tree.lua
+++ lua/tree.lua
@@ -1,6 +1,6 @@
 local configs = require'nvim-treesitter.configs'
 configs.setup {
-	ensure_installed = {"c", "css", "html", "javascript", "typescript", "lua", "python", "bash", "go"}, -- Only use parsers that are maintained
+	ensure_installed = {"c", "css", "html", "javascript", "typescript", "lua", "python", "bash", "go", "latex"}, -- Only use parsers that are maintained
 	highlight = { -- enable highlighting
 		enable = true, 
 	},
